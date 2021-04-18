f0aba3f394a014a621dd8ee45786aeb39c5d3add
kowalkos <79119086+kowalkos@users.noreply.github.com>
2021-04-18 14:52:30 +0200

    'wprowadzenie'

--- otwieranie.py
+++ otwieranie.py
@@ -1,7 +1,13 @@
 import easygui
 import pandas as pd
+from mpl_toolkits.mplot3d import Axes3D
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.graph_objects as go
+from itertools import product, combinations
+"""
 def odczyt_pliku():
-    path = easygui.fileopenbox(filetypes=["*.xlsx",".csv"],default="*.xlsx""*.csv")
+    path = easygui.fileopenbox(filetypes=["*.xlsx",".csv"])
     print(path)
     dane=None
     a=path.split('.')
@@ -20,3 +26,23 @@ def odczyt_pliku():
             return dane[i:]
 dane=odczyt_pliku()
 print(dane)
+
+for index,row in dane.iterrows():
+    print(row[0],row[1])
+
+
+    
+    
+
+
+"""
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+u = np.linspace(0, 2 * np.pi, 100)
+v = np.linspace(0, np.pi, 100)
+x=10*np.arange(0,1,0.01)
+y=10*np.arange(0,1,0.01)
+z = 10*np.outer(np.ones(np.size(u)), np.cos(v))
+ax.plot_surface(x,y,z,color='b')
+plt.show()
+
